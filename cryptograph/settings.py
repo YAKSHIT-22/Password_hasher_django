4d2c57c959d40e5c935c105661dc71a5f6c86201
Yakshit <92084737+YAKSHIT-22@users.noreply.github.com>
2022-09-04 03:06:22 +0530

    Update settings.py

--- cryptograph/settings.py
+++ cryptograph/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qkecyagu3nfdm^wr)g06+m_a1!%ff2u&)c7_vxi94j43+8_%y3'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
